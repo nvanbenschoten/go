f2af0f591dc00e5d063463fb3f200d36f2bf180c
Daniel Morsing <daniel.morsing@gmail.com>
2015-11-10 17:06:41 +0100

    first pass
    includes everything, sort of works. might be slow.
    
    Change-Id: Iec36dc2a1da4c7b801f6b197d639ff63381c2aa5

--- src/runtime/runtime2.go
+++ src/runtime/runtime2.go
@@ -437,6 +437,9 @@ type g struct {
 	timer          *timer         // cached timer for time.Sleep
 	selectDone     uint32         // are we participating in a select and did someone win the race?
 
+	// causal profiling delay
+	causalprofdelay uint64
+
 	// Per-G GC state
 
 	// gcAssistBytes is this G's GC assist credit in terms of
