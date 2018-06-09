aa942cd54fb095985cede3444ee0ab89a1567d32
Daniel Morsing <daniel.morsing@gmail.com>
2018-06-09 13:45:08 +0100

    Add extFiles for runtime/causalprof

--- src/cmd/go/internal/work/gc.go
+++ src/cmd/go/internal/work/gc.go
@@ -79,7 +79,7 @@ func (gcToolchain) gc(b *Builder, a *Action, archive string, importcfg []byte, s
 	extFiles := len(p.CgoFiles) + len(p.CFiles) + len(p.CXXFiles) + len(p.MFiles) + len(p.FFiles) + len(p.SFiles) + len(p.SysoFiles) + len(p.SwigFiles) + len(p.SwigCXXFiles)
 	if p.Standard {
 		switch p.ImportPath {
-		case "bytes", "internal/poll", "net", "os", "runtime/pprof", "runtime/trace", "sync", "syscall", "time":
+		case "bytes", "internal/poll", "net", "os", "runtime/causalprof", "runtime/pprof", "runtime/trace", "sync", "syscall", "time":
 			extFiles++
 		}
 	}
